8aa5fa915e72641708d1c4a45f75699816ac00c5
jisoo7336 <93235120+jisoo7336@users.noreply.github.com>
2022-01-24 17:57:07 +0900

    2022.01.24

---
+++ js/bnbox.js
@@ -0,0 +1,11 @@
+$(function(){
+    let w=window.innerWidth;
+    function slide(){
+      $('.slide').stop().animate({marginLeft:-w}, 2000,function(){
+      $('.slide li:first').appendTo('.slide'); 
+      $('.slide').css({marginLeft:0}); 
+      }); 
+      }
+    
+      setInterval(slide, 10000);
+  });
