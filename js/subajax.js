82928df1442f0b9ec0abc889c3f7ca50e614ac57
jisoo7336 <93235120+jisoo7336@users.noreply.github.com>
2022-01-27 17:32:33 +0900

    2022.01.27

--- js/subajax.js
+++ js/subajax.js
@@ -22,7 +22,28 @@ $.ajax({
 $.ajax({
   method: "GET",
   url: "https://dapi.kakao.com/v3/search/book?target=title",
-  data: { query: "ARTS" },
+  data: { query: "위쳐" },
+ headers: { Authorization: "KakaoAK 49589a251b92786e2edf497d0092b009" }
+})
+  .done(function (msg) {
+  
+    var boxs = document.getElementsByClassName('boxa');
+  
+    for (var i = 0; i < boxs.length; i++) {
+  
+      $("#listf >.boxa").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+      
+    }
+  
+});
+
+
+
+
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "해리포터" },
  headers: { Authorization: "KakaoAK 49589a251b92786e2edf497d0092b009" }
 })
   .done(function (msg) {
@@ -31,16 +52,16 @@ $.ajax({
   
     for (var i = 0; i < boxs.length; i++) {
   
-      $("#liste >.boxe").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
-      $("#liste >.boxe").eq(i).append("<h3>" + '<a href="sub2.html">' + msg.documents[i].title + "</a>" + "</h3>");
-      $("#liste >.boxe").eq(i).append("<h6>" + msg.documents[i].authors + "</h6>");
+      $("#liste >.box").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>");
+      $("#liste >.box").eq(i).append("<h3>" + '<a href="sub2.html">' + msg.documents[i].title + "</a>" + "</h3>");
+      $("#liste >.box").eq(i).append("<h6>" + msg.documents[i].authors + "</h6>");
   
   
       var str = msg.documents[i].contents;
       var str2 = str.substring(0, 60);
   
-      $("#liste >.boxe").eq(i).append("<p>" + '<a href="sub2.html">' + str2 + "</a>" + "</p>");
-      $("#liste >.boxe").eq(i).append('<a href="#" class="btn">' + "click" + "</a>");
+      $("#liste >.box").eq(i).append("<p>" + '<a href="sub2.html">' + str2 + "</a>" + "</p>");
+      $("#liste >.box").eq(i).append('<a href="#" class="btn">' + "click" + "</a>");
   
     }
   
